248182fa912c820a3d7cc24023b1149ecce6db97
Mmjosh <morarajoshua36@gmail.com>
2023-10-19 15:02:35 +0300

    1st draft of a custom printf

--- _printf.c
+++ _printf.c
@@ -5,59 +5,86 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/**
- * _printf - prints formatted string, doesn't; handle the flag chara-
- * cters, have to reproduce the buffer handling of the C library printf
- * function, have to handle precision, length modifiers and field width.
- * @format: pointer to the string provided to `_printf`.
- * @...: variable number of arguments.
- * Return: the number of characters printed(excluding the null
- * terminator used to end output to strings)
- */
 int _printf(const char *format, ...)
 {
-	va_list v_x;
-	int i = 0;
-	int n = 0; /* number of characters printed */
-	char c;
-	char *str;
+    va_list v_x;
+    int i = 0;
+    int n = 0, len; 
+    char *output = NULL; 
+    int output_size = 0;
+    char c, *str; 
 
-	va_start(v_x, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				c = va_arg(v_x, int);
-				write(1, &c, 1);
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(v_x, char *);
-				if (str)
-				{
-				write(1, str, strlen(str));
-				}
-			}
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-			}
-			else
-			{
-				perror("Error: Unexpected format specifier.");
-				exit(98);
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-		}
-		n++;
-		i++;
-	}
-	va_end(v_x);
-	return (n);
+    va_start(v_x, format);
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                c = va_arg(v_x, int);
+                output = (char *)malloc(output_size + 2); 
+                if (output == NULL) {
+                    perror("Error: Memory allocation failed.");
+                    exit(1);
+                }
+                output[output_size] = c;
+                output[output_size + 1] = '\0';
+                output_size += 2;
+            }
+            else if (format[i] == 's')
+            {
+                str = va_arg(v_x, char *);
+                if (str)
+                {
+                    len = strlen(str);
+                    output = (char *)malloc(output_size + len + 1); 
+                    if (output == NULL) {
+                        perror("Error: Memory allocation failed.");
+                        exit(1);
+                    }
+                    strcpy(output + output_size, str);
+                    output_size += len;
+                }
+            }
+            else if (format[i] == '%')
+            {
+                output = (char *)malloc(output_size + 2); 
+                if (output == NULL) {
+                    perror("Error: Memory allocation failed.");
+                    exit(1);
+                }
+                output[output_size] = '%';
+                output[output_size + 1] = '\0';
+                output_size += 2;
+            }
+            else
+            {
+                perror("Error: Unexpected format specifier.");
+                exit(98);
+            }
+        }
+        else
+        {
+            output = (char *)malloc(output_size + 2); 
+            if (output == NULL) {
+                perror("Error: Memory allocation failed.");
+                exit(1);
+            }
+            output[output_size] = format[i];
+            output[output_size + 1] = '\0';
+            output_size += 2;
+        }
+        i++;
+    }
+    va_end(v_x);
+
+    
+    write(1, output, output_size - 1); 
+
+    
+    free(output);
+
+    return n;
 }
+
